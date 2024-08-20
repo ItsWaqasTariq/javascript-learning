38c6d4f94e603942283e5031f57e39d671cc129b
ItsWaqasTariq <waqastariq0078@gmail.com>
2024-08-20 16:38:45 +0500

    jscript is updated

--- first.js
+++ first.js
@@ -1,81 +1,81 @@
-// console.log("start js on vs code");
-// console.log("i linked js file to firefox brower by html file");
-// name="WAQAS TARIQ"
-// console.log("name");
-// console.log(name);                              
-// age=22
-// console.log("age");
-// console.log(age);
-// priceofpen=45.44
-// console.log("price of pen");
-// console.log(priceofpen);
-// x=null
-// console.log(x);
-// y=undefined
-// console.log(y);
-// isLearnig=true;
-// console.log(isLearnig);
-// aboutjs="javascript is dynamical lagu"
-// console.log(aboutjs);
-// const heightt=6;
+console.log("start js on vs code");
+console.log("i linked js file to firefox brower by html file");
+name="WAQAS TARIQ"
+console.log("name");
+console.log(name);                              
+age=22
+console.log("age");
+console.log(age);
+priceofpen=45.44
+console.log("price of pen");
+console.log(priceofpen);
+x=null
+console.log(x);
+y=undefined
+console.log(y);
+isLearnig=true;
+console.log(isLearnig);
+aboutjs="javascript is dynamical lagu"
+console.log(aboutjs);
+const heightt=6;
+console.log(heightt);
+// heightt=7;
 // console.log(heightt);
-// // heightt=7;
-// // console.log(heightt);
-// let a;
-// console.log(a);
-// {
-//     let a=3;
-//    console.log(a);
-// }
-// {
-//     let b=5;
-//    console.log(b);
-// }
+let a;
+console.log(a);
+{
+    let a=3;
+   console.log(a);
+}
+{
+    let b=5;
+   console.log(b);
+}
 
-// // primitive and non-primitive data types
+// primitive and non-primitive data types
 
-// let student={
-//     fullName:"waqas tariq",
-//     age:22,
-//     haircolor:"blackish",
-//     height:6 ,
-//     ismature:"true"
+let student={
+    fullName:"waqas tariq",
+    age:22,
+    haircolor:"blackish",
+    height:6 ,
+    ismature:"true"
 
-// };
-// console.log(student)
-// console.log(student.age)
-// console.log(student["age"])
-// console.log(student.fullName)
-// console.log(student["fullName"])
+};
+console.log(student)
+console.log(student.age)
+console.log(student["age"])
+console.log(student.fullName)
+console.log(student["fullName"])
 
-// let product={
-//     name:"pen",
-//     rating:  "4 star" ,
-//     Deal:"5% off ",
-//     PKR:"270"
-//     // isFollow : false
+let product={
+    name:"pen",
+    rating:  "4 star" ,
+    Deal:"5% off ",
+    PKR:"270"
+    // isFollow : false
    
 
-// };
-// console.log(product)
-// console.log(product.rating)
-// console.log(product["PKR"])
-// console.log(product.Deal)
-// console.log(product["Deal"])
-// console.log(product.name)
-// console.log(product["name"])
+};
+console.log(product)
+console.log(product.rating)
+console.log(product["PKR"])
+console.log(product.Deal)
+console.log(product["Deal"])
+console.log(product.name)
+console.log(product["name"])
 
-// console.log(typeof product["name"])
-// console.log(typeof product.rating)
-// console.log(typeof product)
+console.log(typeof product["name"])
+console.log(typeof product.rating)
+console.log(typeof product)
 
-// // arithmetic operator
+// arithmetic operator
 
-// // let x=2;
-// // let y=5;
-// // console.log(x ** y);
+// let x=2;
+// let y=5;
+// console.log(x ** y);
 
-// operators in JS
+operators in JS
 
 // equal to ==
 // not equal to !=
